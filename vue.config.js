fa5f5b488cdc56fca3725254c8ce3ac26e27bbe2
TyT|xexebe <aspipaht007@gmail.com>
2023-11-07 18:49:47 +0200

    fixed vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/mlog-learn-vue/" : "/",
-  transpileDependencies: true
-});
+module.exports = {
+  publicPath: "/mlog-learn-vue/",
+};
