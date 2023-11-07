7f1784db67c7b773b9c5804b99b99a72a66ec425
TyT|xexebe <aspipaht007@gmail.com>
2023-11-07 22:47:42 +0200

    router changes

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,13 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
 import About from '@/views/About.vue';
 
+const routes = [
+  {
+    path: '/about', name: 'About', component: About
+  },
+]
+
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/about', component: About
-    },
-  ]
-});
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
 
 export default router;
