55e60ec16b475fa0b4d1964c1987e02eb7197aa6
TyT|xexebe <aspipaht007@gmail.com>
2023-11-07 20:10:18 +0200

    deploy

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,12 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import About from '@/views/About.vue';
-
+import Main from './../../App.vue';
 
 const router = createRouter({
   history: createWebHistory('/mlog-learn-vue/'),
   routes: [
     {
       path: '/about', component: About
+    },
+    {
+      path: '/', component: Main
     }
   ]
 });
