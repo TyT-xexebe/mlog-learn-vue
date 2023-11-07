46729f2f2dc7f8f3797b5214fae7194218d3bb06
TyT|xexebe <aspipaht007@gmail.com>
2023-11-07 22:09:48 +0200

    trying anothey way to routing in gh-pages

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,16 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import About from '@/views/About.vue';
-import Main from './../../App.vue';
 
 const router = createRouter({
-  history: createWebHistory('/mlog-learn-vue/'),
+  history: createWebHistory(),
   routes: [
     {
       path: '/about', component: About
     },
-    {
-      path: '/', component: Main
-    }
   ]
 });
 
--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,5 @@
-
+const { defineConfig } = require('@vue/cli-service');
+module.exports = defineConfig({
+  publicPat: '/mlog-learn-vue/',
+  transpileDependencies: true
+})
