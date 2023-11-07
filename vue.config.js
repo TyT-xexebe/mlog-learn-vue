ecb524b74dc0372b91ea4e92ec81ebc3776444fd
TyT|xexebe <aspipaht007@gmail.com>
2023-11-07 22:24:32 +0200

    fixing..

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  publicPat: '/mlog-learn-vue/',
+  publicPath: '/mlog-learn-vue/',
   transpileDependencies: true
 })
