39e160cf319b735b4608c642f8d06113ddd61136
TyT|xexebe <aspipaht007@gmail.com>
2023-11-07 18:23:48 +0200

    fixed router

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <router-view></router-view>
+  <nav-bar></nav-bar>
+  <footer-bar></footer-bar>
 </template>
 <script>
-
+import navBar from '@/components/nav-bar.vue'
+import footerBar from '@/components/footer.vue'
+export default {
+  components: {
+    'nav-bar': navBar,
+    'footer-bar': footerBar,
+  }
+}
 </script>
 <style lang="scss">
   // default styles reset
--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="nav-bar" ref="nav">
     <div class="left">
-      <my-button to="/about" :class="{ active: $route.path === '/about' }">About</my-button>
-      <my-button to="/basic" :class="{ active: $route.path === '/basic' }">Basic</my-button>
+      <my-button @click="$router.push('/about')" :class="{ active: $route.path === '/about' }">About</my-button>
+      <my-button @click="$router.push('/basic')" :class="{ active: $route.path === '/basic' }">Basic</my-button>
     </div>
 
     
       <div class="right" ref="navRight">
-        <my-button to="/commands" :class="{ active: $route.path === '/commands' }">Commands</my-button>
-        <my-button to="/examples" :class="{ active: $route.path === '/examples' }">Code Examples</my-button>
-        <my-button to="/debugger" :class="{ active: $route.path === '/debugger' }">Debugger</my-button>
-        <my-button to="/login" :class="{ active: $route.path === '/login' }">Log In</my-button>
+        <my-button @click="$router.push('/commands')" :class="{ active: $route.path === '/commands' }">Commands</my-button>
+        <my-button @click="$router.push('/examples')" :class="{ active: $route.path === '/examples' }">Code Examples</my-button>
+        <my-button @click="$router.push('/debugger')" :class="{ active: $route.path === '/debugger' }">Debugger</my-button>
+        <my-button @click="$router.push('/login')" :class="{ active: $route.path === '/login' }">Log In</my-button>
       </div>
     
     <div class="menu-btn-container" @click="openFullNav">
